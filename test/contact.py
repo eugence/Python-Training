0870c1e46b49937ac8ca296fd452cb6a0218192d
eugence <eugence@mail.ru>
2017-02-02 13:54:56 +0300

    Задание 3

---
+++ test/contact.py
@@ -0,0 +1,13 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, nickname, title, company_name, address1, email, birth_year, address2):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.nickname = nickname
+        self.title = title
+        self.company_name = company_name
+        self.address1 = address1
+        self.email = email
+        self.birth_year = birth_year
+        self.address2 = address2
